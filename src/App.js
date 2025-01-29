6ce35bcfabf2c1195c5a04671875ff5c070f19d1
Odkoo Ch <odkoo@Odkoos-MacBook-Air.local>
2025-01-28 23:48:41 -0600

    update App.js

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Hello World!! Od-Erdene</h1>
+      <h1>Hello World!! Od-Erdene Chuluunsukh</h1>
     </div>
   );
 }
